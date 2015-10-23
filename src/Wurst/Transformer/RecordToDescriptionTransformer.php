fe770c11dbc715742812fa1579a809b9e95d616a
Alex Woroschilow <alex.woroschilow@gmail.com>
2015-10-23 17:47:29 +0200

    patch description transformer

--- src/Wurst/Transformer/RecordToDescriptionTransformer.php
+++ src/Wurst/Transformer/RecordToDescriptionTransformer.php
@@ -3,10 +3,17 @@
 namespace Wurst\Transformer;
 
 use Wurst\History\Entity\Record;
+
 class RecordToDescriptionTransformer {
 	public function __construct() {
 	}
-	public function transform(Record $record) {
-		return str_replace("\n", "<br/>", $record->getInfo());
+	public function transform(Record $element) {
+		$element->setError ( str_replace ( "\n", "<br/>", $element->getError () ) );
+		$element->setError ( str_replace ( ",", ", ", $element->getError () ) );
+		
+		$element->setFatal ( str_replace ( "\n", "<br/>", $element->getFatal () ) );
+		$element->setFatal ( str_replace ( ",", ", ", $element->getFatal () ) );
+		
+		return "{$element->getFatal()} {$element->getError()}";
 	}
 }
